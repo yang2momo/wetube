22535249bc3da235ec6b5d09528c85664be9d351
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-22 01:43:26 +0900

    #2.10

--- wetubeStudy/app.js
+++ wetubeStudy/app.js
@@ -4,12 +4,14 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-Parser";
 
-import { userRouter } from "./router.js";
+import routes from "./routes"
+
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my momo");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,8 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 export default app;
