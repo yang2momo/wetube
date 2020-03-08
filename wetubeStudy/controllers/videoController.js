978de3b49116b0e577528fb099cf603f43333539
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-03-09 05:23:48 +0900

    #3.11

--- wetubeStudy/controllers/videoController.js
+++ wetubeStudy/controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
     try {
-        const videos = await Video.find({});
+        const videos = await Video.find({}.sort({ '_id': -1 }));
         res.render("home", { pageTitle: "Home", videos });
     } catch (error) {
         console.log(error);
@@ -15,11 +15,12 @@ export const search = (req, res) => {
     const {
         query: { term: searchingBy }
     } = req;
-    res.render("search", { pageTitle: "Search", searchingBy, videos });
+    res.render("search", { pageTitle: "Search", searchingBy });
 };
 
-export const getUpload = (req, res) =>
-    res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" })
+};
 
 export const postUpload = async (req, res) => {
     const {
@@ -78,6 +79,7 @@ export const deleteVideo = async (req, res) => {
     try {
         await Video.findOneAndRemove({ _id: id });
     } catch (error) {
+        console.log(error);
     }
     res.redirect(routes.home);
 
